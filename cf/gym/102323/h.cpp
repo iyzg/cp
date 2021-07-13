dc1724a362867aa0cd033cc944b0574554995252
Ivy Zhang <ZIvy042003@gmail.com>
2021-07-13 13:32:47 -0400

    update

--- cf/gym/102323/h.cpp
+++ cf/gym/102323/h.cpp
@@ -1,3 +1,7 @@
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -8,74 +12,101 @@ typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 
-int te = 1;
-int c;
-set<char> shapes[3][3], flavors[3][3];
+int te = 1, c;
 
-void solve() {
-    cin >> c;
-    // Reset
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 3; ++j) {
-            shapes[i][j].insert('S');
-            shapes[i][j].insert('R');
-            shapes[i][j].insert('T');
-            flavors[i][j].insert('S');
-            flavors[i][j].insert('V');
-            flavors[i][j].insert('C');
+vector<string> pos = { "RC", "RS", "RV" ,"SC", "SS", "SV", "TC", "TS", "TV" };
+bool used[9];
+vector<vector<vector<string>>> clues;
+string board[3][3];
+
+bool solve(int row, int col) {
+    if (row == 3) {
+        for (auto clue : clues) {
+            bool fitClue = false;
+            for (int bx = 0; bx < 3; ++bx) {
+                for (int by = 0; by < 3; ++by) {
+                    if (bx + sz(clue) > 3 || by + sz(clue[0]) > 3) continue;
+
+                    fitClue = true;
+                    for (int cx = 0; cx < sz(clue); ++cx) {
+                        for (int cy = 0; cy < sz(clue[0]); ++cy) {
+                            if (clue[cx][cy][0] != '_' && clue[cx][cy][0] != board[bx + cx][by + cy][0]) fitClue = false;
+                            if (clue[cx][cy][1] != '_' && clue[cx][cy][1] != board[bx + cx][by + cy][1]) fitClue = false;
+                        }
+                    }
+                    if (fitClue) break;
+                }
+                if (fitClue) break;
+            }
+
+            if (!fitClue) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    for (int i = 0; i < 9; ++i) {
+        if (!used[i]) {
+            used[i] = true;
+            board[row][col] = pos[i];
+            bool res = solve((3 * row + col + 1) / 3, (col + 1) % 3);
+            if (res) return true;
+            used[i] = false;
         }
     }
 
+    return false;
+}
+
+void solve() {
+    cin >> c;
+
+    clues.clear();
     for (int i = 0; i < c; ++i) {
         int x, y; cin >> x >> y;
-        vector<pair<int, int>> val;
-        for (int tx = 0; tx < 3; ++tx) {
-            for (int ty = 0; ty < 3; ++ty) {
-                if (tx + x <= 3 && ty + y <= 3) {
-                    val.push_back({tx, ty});
-                }
+        vector<vector<string>> clue(x, vector<string>(y));
+        for (int cx = 0; cx < x; ++cx) {
+            for (int cy = 0; cy < y; ++cy) {
+                cin >> clue[cx][cy];
             }
         }
 
-        for (int cx = 0; cx < x; ++cx) {
-            for (int cy = 0; cy < y; ++cy) {
-                string clue; cin >> clue;
+        clues.push_back(clue);
+    }
 
-                for (auto p : val) {
-                    int nx = p.first + cx, ny = p.second + cy;
-                    if (clue[0] != '_') {
-                        auto it = shapes[nx][ny].begin();
-                        while (it != shapes[nx][ny].end()) {
-                            auto nxt = next(it);
-                            if (*it != clue[0]) shapes[nx][ny].erase(it);
+    solve(0, 0);
 
-                            it = nxt;
-                        }
-                    }
-
-                    if (clue[1] != '_') {
-                        auto it = flavors[nx][ny].begin();
-                        while (it != flavors[nx][ny].end()) {
-                            auto nxt = next(it);
-                            if (*it != clue[1]) flavors[nx][ny].erase(it);
-
-                            it = nxt;
-                        }
-
+    int cnt = 0;
+    for (auto clue : clues) {
+        ++cnt;
+        bool fitClue = false;
+        for (int bx = 0; bx < 3; ++bx) {
+            for (int by = 0; by < 3; ++by) {
+                if (bx + sz(clue) > 3 || by + sz(clue[0]) > 3) {
+                    fitClue = false;
+                    continue;
+                }
+                fitClue = true;
+                for (int cx = 0; cx < sz(clue); ++cx) {
+                    for (int cy = 0; cy < sz(clue[0]); ++cy) {
+                        if (clue[cx][cy][0] != '_' && clue[cx][cy][0] != board[bx + cx][by + cy][0]) fitClue = false;
+                        if (clue[cx][cy][1] != '_' && clue[cx][cy][1] != board[bx + cx][by + cy][1]) fitClue = false;
                     }
                 }
+                if (fitClue) {
+                    cout << "Clue #" << cnt << " worked on " << bx << ", " << by << '\n';
+                    break;
+                }
             }
+            if (fitClue) break;
         }
     }
 
     cout << "Puzzle #" << te << ": \n";
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
-            // cout << *shapes[i][j].begin() << *flavors[i][j].begin() << ' ';
-            for (auto x : shapes[i][j]) cout << x;
-            cout << ',';
-            for (auto x : flavors[i][j]) cout << x;
-            cout << ' ';
+            cout << board[i][j] << ' ';
         }
         cout << '\n';
     }
